6c8d8289e292b100dad2a407c91cd257cb6e8089
nikita-mirchandani <82458994+nikita-mirchandani@users.noreply.github.com>
2021-10-31 18:55:25 +0530

    Add files via upload

---
+++ react-signup form/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import Signup from './components/Signup';
+
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Welcome to Nikita's React session</h1>
+    <Signup/>
+    </div>
+  );
+}
+
+export default App;
